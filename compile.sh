0894ba712c595adf6ad22ae4c0f4d4a4955bc824
Yigong Hu <hyigong1@jhu.edu>
2022-09-29 20:31:29 -0400

    add compile

--- compile.sh
+++ compile.sh
@@ -3,7 +3,6 @@
 mkdir -p dist
 cd 2.4.38
 
-./buildconf
 ./configure --prefix=$(pwd)/../dist --with-included-apr --enable-so --enable-cgi --with-mpm=prefork
 
 make -j4
@@ -11,15 +10,24 @@ if [ $? -ne 0 ]; then
     echo "Failed to build apache"
     exit 1
 fi
-
 make install
 
+git submodule update --init --recursive
 cd perfIsolation-Apache-mod_fcgid
 APXS=$(pwd)/../dist/bin/apxs ./configure.apxs
 make
+if [ $? -ne 0 ]; then
+    echo "Failed to build apache"
+    exit 1
+fi
 make install
 
 cd php-7.4.23
+./buildconf --force
 ./configure --with-apxs2=$(pwd)/../dist/bin/apxs --prefix=$(pwd)/../dist/php
-make 
+make -j4 
+if [ $? -ne 0 ]; then
+    echo "Failed to build apache"
+    exit 1
+fi
 make install
